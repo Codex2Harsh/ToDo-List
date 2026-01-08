7021cf3a78220d5ebd302418b0934e053a5001f0
Harsh Dwivedi <thecody1209@gmail.com>
2026-01-08 15:43:04 +0530

    Added localstorage funcitonality to add and delete buttons.

--- todo.js
+++ todo.js
@@ -3,10 +3,6 @@ let todoList= [
     {
     item:'Buy Milk', 
     dueDate:'6/1/2025'
-    },
-    {
-    item:'Go to college', 
-    dueDate:'6/1/2025'
     }, 
     {
     item:'complete homework',
@@ -23,6 +19,7 @@ function addTask(){
         item:inputElement.value , 
         dueDate:inputDate.value
     });
+    localStorage.setItem('todoList',JSON.stringify(todoList));
     inputElement.value='';
     inputDate.value='';
     displayItems();
@@ -33,9 +30,23 @@ function formatDate(dateStr) {
     let date = new Date(dateStr);
     return date.toLocaleDateString(); // auto formats for user's region
 }
+function deleteTask(item){
+   todoList = todoList.filter(itemname => itemname != item);
+   localStorage.setItem('todoList',JSON.stringify(todoList));
+   displayItems();
+}
 
 function displayItems(){
     let display = document.querySelector('.container-items');
+    //adding the enter list to localStorage 
+    let listStr = localStorage.getItem('todoList');
+    //if todolist exists then display wid added item
+    todoList = todoList ? JSON.parse(listStr):todoList;
+    //above is if a dummy data is present in the code
+    //if it is not present then the code will be 
+    // todoList = listStr ? JSON.parse(listStr):[];
+    //to handle if todolist is empty
+    if(!todoList){return;}
     let newHtml='';
     for(let i=0; i<todoList.length; i++){
         let{item,dueDate}= todoList[i];
@@ -43,7 +54,7 @@ function displayItems(){
         <span>${item}</span>
         <span>${formatDate(dueDate)}</span>
         <button class= "delete-btn" onclick="
-        todoList.splice(${i},1);
+        deleteTask(todoList[${i}]);
         displayItems(); 
         "> Delete </button>`;
     }
